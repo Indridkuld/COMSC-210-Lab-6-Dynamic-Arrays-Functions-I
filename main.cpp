2d5de32b41e4d90f839a2927331aaacb9a5e1a5e
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-14 10:42:50 -0700

    update

--- main.cpp
+++ main.cpp
@@ -7,32 +7,34 @@ using namespace std;
 
 // Function prototypes
 
-double enterArrayData(double* arr[], const int size);
-int outputArrayData(double* arr[], const int size);
-void sumArray(double* arr[], const int size);
+double enterArrayData(double arr[], const int size);
+int outputArrayData(double arr[], const int size);
+void sumArray(double arr[], const int size);
 
 int main() {
     const int SIZE = 5;
     double *ddArr= new double[SIZE];
     double sum = 0.0;
 
-    enterArrayData(*ddArr, SIZE);
+    enterArrayData(ddArr, SIZE);
+    outputArrayData(ddArr, SIZE);
+    sumArray(ddArr, SIZE);
 
 
     return 0;
 }
 
 // Function definitions
-double enterArrayData(double* arr[], const int size) {
+double enterArrayData(double arr[], const int size) {
 
     return 0.0;
 }
 
-int outputArrayData(double* arr[], const int size) {
+int outputArrayData(double arr[], const int size) {
 
     return 0;
 }
 
-void sumArray(double* arr[], const int size) {
+void sumArray(double arr[], const int size) {
 
 }
