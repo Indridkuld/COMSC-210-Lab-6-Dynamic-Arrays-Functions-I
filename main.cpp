591788f0dc50852e9fff95a457b4dd68105949ef
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-14 10:07:07 -0700

    update

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// COMSC-210 | Lab 6 pt 1 | Aidan Woodcock | 2024-09-14
+// IDE used: Visual Studio Code
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Function prototypes
+
+double enterArrayData();
+int outputArrayData();
+void sumArray(); 
+
+int main() {
+
+    return 0;
+}
+
+// Function definitions
