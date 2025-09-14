5165dbe655fef9ffe6d8f01d74edb35960d04132
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-14 11:28:26 -0700

    final commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// COMSC-210 | Lab 6 pt 1 | Aidan Woodcock | 2024-09-14
+// COMSC-210 | Lab 6 pt 1 | Aidan Woodcock | 2025-09-14
 // IDE used: Visual Studio Code
 #include <iostream>
 #include <iomanip>
@@ -6,7 +6,6 @@
 using namespace std;
 
 // Function prototypes
-
 void enterArrayData(double arr[], const int size);
 void outputArrayData(double arr[], const int size);
 void sumArray(double arr[], const int size, double &sum);
@@ -14,35 +13,36 @@ void sumArray(double arr[], const int size, double &sum);
 int main() {
     const int SIZE = 5;
     double *ddArr = nullptr;
-    ddArr= new double[SIZE];
+    ddArr = new double[SIZE]; // dynamically allocate array
     double sum = 0.0;
 
     enterArrayData(ddArr, SIZE);
     outputArrayData(ddArr, SIZE);
     sumArray(ddArr, SIZE, sum);
-    cout << "\n Sum of Values: " << sum << endl;
+    cout << "Sum of Values: " << sum << endl; 
 
-    delete[] ddArr;
+    delete[] ddArr; // free dynamically allocated memory
 
     return 0;
 }
-
 // Function definitions
+// enters array data through console input using pointers. lists elements from 1-5 for user friendliness
 void enterArrayData(double arr[], const int size) {
+    cout << "Data entry for the array: \n" << endl;
     for (int i = 0; i < size; i++) {
         cout << " Enter value for element " << (i + 1) << ": " << endl;
         cin >> *(arr + i);
     };
     cout << "Data entry complete. " << endl;
 }
-
+// outputs array data using pointers. outputs simple spaced list 
 void outputArrayData(double arr[], const int size) {
-    cout << " Outputting array elements: " << endl;
+    cout << "Outputting array elements: " << endl;
     for (int i = 0; i < size; i++) // output array using pointers
         cout << *(arr + i) << " ";
     cout << endl;
 }
-
+// sums array data using pointers. returns sum by reference
 void sumArray(double arr[], const int size, double &sum) {
     for (int i = 0; i < size; i++) {
         sum += *(arr + i);
